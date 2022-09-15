c3166ec8257eea4da07278828d8db00aca705d0f
Seonguk <xsungwook3x@naver.com>
2022-09-15 22:08:35 +0900

    Add : SDDS intro page navbar , js and css and utils's button

---
+++ src/components/IntroPage/Navbar/Navbar.js
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import Button from '../../utils/Button';
+import './Navbar.css';
+
+function Navbar(){
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    return (
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/' className='navbar-logo' >
+                    SDDS
+                    <i class="fa-solid fa-code"></i>
+                </Link>
+                <div className='menu-icon'>
+                    <i className = {click ? 'fas fa-times' : 'fas fa-bars' } />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-link'>주요기능</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-link'>템플릿</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links-moblie'>
+                            Login
+                        </Link>
+                    </li>
+                    
+
+                </ul> 
+                <Button buttonStyle='btn--pink' buttonSize='btn--medium' name='Login'>Login</Button>
+                <Button buttonStyle='btn--white' buttonSize='btn--medium' name='시작하기'>시작하기</Button>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar;
