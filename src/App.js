eff7884564c7f7ddc7fc6aca9ebed606a13009b5
Seonguk <xsungwook3x@naver.com>
2022-09-21 13:55:29 +0900

    이건 test 무시하셈

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<IntroPage/>}/>        
+          <Route path='/' element={<IntroPage/>}/>
+          <Route path='/aaa' element={<h1>hi</h1>}></Route>        
         </Routes>
       </BrowserRouter>
       
