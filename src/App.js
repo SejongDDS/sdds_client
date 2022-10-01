7a2e98c1924282494b657ef1f874a5f03f4319e2
전진우 <jinu@M2A1.local>
2022-10-01 14:01:17 +0900

    modify toolbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Modal from "react-modal";
 import "./App.css";
-import { BrowserRouter, Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IntroPage from "./components/IntroPage/IntroPage";
 import SignUpPage from "./components/SignUpPage/SignUpPage";
 import MainPage from "./components/MainPage/MainPage";
@@ -17,7 +17,6 @@ function App() {
                     <Route path="/signup" element={<SignUpPage />} />
                 </Routes>
             </BrowserRouter>
-            
         </div>
     );
 }
