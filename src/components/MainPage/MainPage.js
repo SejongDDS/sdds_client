fa153dd7d86d3f4152b928d76f7c150d0f4fe321
전진우 <jinu@M2A1.local>
2022-10-23 22:37:01 +0900

    custom backup

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -24,6 +24,10 @@ import Sidebar from "./Pages/Sidebar";
 import TopNav from "./Pages/TopNav";
 import PageSection from "./util/PageSection";
 
+// html에 스크립트 들어가는거 확인
+// 그래프ql -> rest로 바꿔달라고?, 리코일
+// 플러그인 - 페이지
+
 function MainPage() {
     const [editor, setEditor] = useState(null);
     // const [assets, setAssets] = useState([]);
@@ -71,55 +75,55 @@ function MainPage() {
 
         addCommands(editor, domain);
 
-        editor.on("run:preview", () => {
-            console.log("It will trigger when we click on preview icon");
-            // This will be used to hide border
-            editor.stopCommand("sw-visibility");
-            // This will hide the sidebar view
-            navbar.removeClass("sidebar");
-            // This will make the main-content to be full width
-            mainContent.removeClass("main-content");
+        // editor.on("run:preview", () => {
+        //     console.log("It will trigger when we click on preview icon");
+        //     // This will be used to hide border
+        //     editor.stopCommand("sw-visibility");
+        //     // This will hide the sidebar view
+        //     navbar.removeClass("sidebar");
+        //     // This will make the main-content to be full width
+        //     mainContent.removeClass("main-content");
 
-            // This will hide top panel where we have added the button
-            panelTopBar.addClass("d-none");
-        });
+        //     // This will hide top panel where we have added the button
+        //     panelTopBar.addClass("d-none");
+        // });
 
-        editor.on("stop:preview", () => {
-            // This event is reverse of the above event.
-            console.log("It will trigger when we click on cancel preview icon");
-            editor.runCommand("sw-visibility");
-            navbar.addClass("sidebar");
-            mainContent.addClass("main-content");
-            panelTopBar.removeClass("d-none");
-        });
+        // editor.on("stop:preview", () => {
+        //     // This event is reverse of the above event.
+        //     console.log("It will trigger when we click on cancel preview icon");
+        //     editor.runCommand("sw-visibility");
+        //     navbar.addClass("sidebar");
+        //     mainContent.addClass("main-content");
+        //     panelTopBar.removeClass("d-none");
+        // });
 
-        editor.on("component:selected", (component) => {
-            const newTool = {
-                icon: "fa fa-plus-square",
-                title: "Check Toolbar",
-                commandName: "new-tool-cmd",
-                id: "new-tool",
-            };
+        // editor.on("component:selected", (component) => {
+        //     const newTool = {
+        //         icon: "fa fa-plus-square",
+        //         title: "Check Toolbar",
+        //         commandName: "new-tool-cmd",
+        //         id: "new-tool",
+        //     };
 
-            const defaultToolbar = component.get("toolbar");
-            const checkAlreadyExist = defaultToolbar.find(
-                (toolbar) => toolbar.command === newTool.commandName
-            );
-            if (!checkAlreadyExist) {
-                defaultToolbar.unshift({
-                    id: newTool.id,
-                    attributes: { class: newTool.icon, title: newTool.title },
-                    command: newTool.commandName,
-                });
-                component.set("toolbar", defaultToolbar);
-            }
-        });
+        //     const defaultToolbar = component.get("toolbar");
+        //     const checkAlreadyExist = defaultToolbar.find(
+        //         (toolbar) => toolbar.command === newTool.commandName
+        //     );
+        //     if (!checkAlreadyExist) {
+        //         defaultToolbar.unshift({
+        //             id: newTool.id,
+        //             attributes: { class: newTool.icon, title: newTool.title },
+        //             command: newTool.commandName,
+        //         });
+        //         component.set("toolbar", defaultToolbar);
+        //     }
+        // });
 
-        editor.BlockManager.add("my-first-block", {
-            label: "Simple block",
-            content:
-                '<div><script src="https://code.jquery.com/jquery-3.6.1.slim.min.js"></script></div>',
-        }); //이거 안먹음
+        // editor.BlockManager.add("my-first-block", {
+        //     label: "Simple block",
+        //     content:
+        //         '<div><script src="https://code.jquery.com/jquery-3.6.1.slim.min.js"></script></div>',
+        // }); //이거 안먹음
 
         setTimeout(() => {
             let categories = editor.BlockManager.getCategories();
